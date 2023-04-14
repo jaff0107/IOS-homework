fd1a0aa474dc2535a1d75e4faf508b1f09435c1b
jaff0107 <jaff2532@gmail.com>
2023-04-14 19:07:46 +0800

    Initial Commit

---
+++ IOS Exercise/IOS_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_ExerciseApp.swift
+//  IOS Exercise
+//
+//  Created by Jeff Chiu on 2023/4/14.
+//
+
+import SwiftUI
+
+@main
+struct IOS_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
