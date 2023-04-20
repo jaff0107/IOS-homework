188d6c9b6914f551b5b2c1616b334e952c426c95
jaff0107 <jaff2532@gmail.com>
2023-04-20 20:06:14 +0800

    第10章

---
+++ FoodPin/FoodPin/View/ActivityView.swift
@@ -0,0 +1,25 @@
+//
+//  ActivityView.swift
+//  FoodPin
+//
+//  Created by Jeff Chiu on 2023/4/20.
+//
+
+import SwiftUI
+
+struct ActivityView: UIViewControllerRepresentable {
+    
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        return activityController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        <#code#>
+    }
+    
+    }
+
